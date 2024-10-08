e17956253686b32c9b2b6bf621bf0fad46129bf9
ManuelDa <manuelda27999@gmail.com>
2024-10-08 21:28:38 +0200

    ciclo for

---
+++ stuff/for/for.js
@@ -0,0 +1,9 @@
+console.log("Ejemplo ciclo for")
+
+for (var i = 0; i < 5; i++) {
+    console.log(i)
+}
+
+//Primer espacio = declaración del índice
+//Segundo espacio = condición para ejecutar el código de dentro
+//Tercer espacio = el código tras ejecutar el código de dentro
